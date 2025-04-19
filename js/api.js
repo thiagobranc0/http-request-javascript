d75166fe31d66a77bac945b4d9239d13ceb15f34
thiagobranc0 <thiagobranco48@yahoo.com>
2025-04-19 00:03:23 -0300

    Added get and post methods to api js

---
+++ js/api.js
@@ -0,0 +1,28 @@
+const api = {
+  async buscarPensamentos() {
+    try {
+      const response = await fetch("http://localhost:3000/pensamentos");
+      return await response.json();
+    } catch {
+      alert("Erro ao buscar pensamentos");
+      throw error;
+    }
+  },
+
+  async salvarPensamento(pensamento) {
+    try {
+      const response = await fetch("http://localhost:3000/pensamentos", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(pensamento),
+      });
+    } catch {
+      alert("Erro ao buscar pensamentos");
+      throw error;
+    }
+  },
+};
+
+export default api;
